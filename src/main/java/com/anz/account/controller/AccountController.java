3ed595f3f44b5630ef675072f5307af329a33615
Pankaj <pankaj.hirapara@cognizant.com>
2019-07-03 12:39:20 +1000

    Added info message for accounts/transactions not found.

--- src/main/java/com/anz/account/controller/AccountController.java
+++ src/main/java/com/anz/account/controller/AccountController.java
@@ -1,5 +1,6 @@
 package com.anz.account.controller;
 
+import com.anz.account.exception.ErrorMessage;
 import com.anz.account.model.AccountRes;
 import com.anz.account.service.AccountService;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,6 +32,10 @@ public class AccountController {
     @Value("${accounts.for.user}")
     private String accountInfo;
 
+    @Value("${account.not.found}")
+    private String accountNotFound;
+
+
     @GetMapping(value = "/{userId}" , produces = "application/hal+json")
     public @ResponseBody ResponseEntity<?> getUserAccounts(@RequestParam(value = "page", defaultValue = "0") @PositiveOrZero int page,
                                                    @RequestParam(value = "size", defaultValue = "20") @Max(value = 100) @Positive int size,
@@ -37,9 +43,13 @@ public class AccountController {
 
         List<AccountRes> accounts = accountService.getUserAccounts(userId, PageRequest.of(page, size));
         log.info(accountInfo, accounts.size(), userId);
+        if(accounts.size() > 0){
         Resources<AccountRes> resources = new Resources<AccountRes>(accounts);
         resources.add(linkTo(methodOn(AccountController.class).getUserAccounts(page, size, userId)).withSelfRel());
-        return ResponseEntity.ok().body(resources);
+            return ResponseEntity.ok().body(resources);
+        }else{
+             return ResponseEntity.ok().body( new ErrorMessage(accountNotFound + userId, HttpStatus.OK));
+        }
     }
 
 }
--- src/main/java/com/anz/account/controller/TransactionController.java
+++ src/main/java/com/anz/account/controller/TransactionController.java
@@ -1,6 +1,7 @@
 package com.anz.account.controller;
 
 
+import com.anz.account.exception.ErrorMessage;
 import com.anz.account.model.TransactionRes;
 import com.anz.account.service.AccountService;
 import com.anz.account.service.TransactionService;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,6 +36,9 @@ public class TransactionController {
     @Value("${transactions.for.account}")
     private String transactionInfo;
 
+    @Value("${transactions.not.found}")
+    private String transactionNotFound;
+
     @GetMapping(value = "/{accountNumber}/transactions", produces = "application/hal+json")
     public @ResponseBody ResponseEntity<?> getTransactions(@RequestParam(value = "page", defaultValue = "0") @PositiveOrZero int page,
                                                              @RequestParam(value = "size", defaultValue = "20") @Max(value = 100) @Positive int size,
@@ -41,10 +46,15 @@ public class TransactionController {
 
         List<TransactionRes> transactions = transactionService.getTransactions(accountNumber, PageRequest.of(page, size));
         log.info(transactionInfo, transactions.size(), accountNumber);
+        if(transactions.size() > 0){
         Resources<TransactionRes> resources = new Resources<TransactionRes>(transactions);
         resources.add(linkTo(methodOn(TransactionController.class).getTransactions(page, size, accountNumber)).withSelfRel());
         resources.add(linkTo(methodOn(AccountController.class).getUserAccounts(page, size, accountService.getAccountByAccountNumber(accountNumber).getUserId())).withRel("accounts"));
-        return ResponseEntity.ok().body(resources);
+            return ResponseEntity.ok().body(resources);
+        }else{
+            return ResponseEntity.ok().body( new ErrorMessage(transactionNotFound + accountNumber, HttpStatus.OK));
+        }
+
    }
 
 }
